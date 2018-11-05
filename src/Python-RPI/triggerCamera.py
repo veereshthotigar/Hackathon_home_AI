b027fc5524182ee251336e16677c773dbc3e2c35
veereshthotigar <veeresh.thotygar@gmail.com>
2018-11-05 04:08:05 -0600

    Motion Detection

--- src/Python-RPI/triggerCamera.py
+++ src/Python-RPI/triggerCamera.py
@@ -7,12 +7,6 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(2,GPIO.OUT)
-client = boto3.client(
-	"sns",
-	aws_access_key_id="AKIAJ6CMBFUUV4MKJZQA",
-	aws_secret_access_key="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",
-	region_name="us-east-1"
-)
 camera = PiCamera()
 
 def previewImage():
@@ -28,12 +22,6 @@ def triggerLEDOn():
 def triggerLEDOff():
 	print "LED off"
 	GPIO.output(2,GPIO.LOW)
-
-def triggerNotification():
-	client.publish(
-		Message="there is someone at the door!",
-		TopicArn="arn:aws:sns:us-east-1:920073451805:camera-notifications"
-	)
 	
 while True:
 	time.sleep(5)
